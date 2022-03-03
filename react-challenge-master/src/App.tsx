f505c764ead6412826ee678c7dd29f9fee14fa93
Sachin Kumar <sjun1992@gmail.com>
2022-03-03 20:29:48 +0530

    Initial code

---
+++ react-challenge-master/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { FunctionComponent } from 'react';
+import './App.scss';
+
+const GIT_USER = 'dhh';
+const GIT_USER_URL = `https://api.github.com/users/${GIT_USER}/events`;
+
+type AppState = {};
+
+interface Data {
+  type: string;
+}
+
+
+const scoreMap = new Map<string, number>();
+scoreMap.set('PushEvent', 5);
+scoreMap.set('PullRequestReviewCommentEvent', 4);
+scoreMap.set('WatchEvent', 3);
+scoreMap.set('CreateEvent',2);
+
+const App: FunctionComponent<AppState> = (props) => {
+
+  const getScore = () => {
+    fetch(GIT_USER_URL).then(response => response.json())
+    .then(response => {
+      calculateScore(response); 
+
+    });
+  };
+
+  const calculateScore = (data: Data[]) => {
+    let score = 0;
+    data.forEach((row: Data) => {
+      const type = row.type;
+      score += scoreMap.get(type) ?? 1;
+    });
+    document.getElementsByClassName('score')[0].innerHTML = '' + score;
+  };
+
+  return (
+    <div className="app-container">
+      <div className="score">n/a</div>
+      <button type="button" className="primary-btn" aria-label="Get Score" onClick={getScore}>
+        Get Score
+      </button>
+    </div>
+  );
+};
+
+export default App;
